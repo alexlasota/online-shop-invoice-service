7dcf1fa7e1f387781d4b8f2530ad59d34c04cf40
alexlasota <alexylasota@gmail.com>
2024-09-05 18:01:53 +0200

    .

---
+++ src/main/java/com/alexlasota/online_shop_invoice_service/OnlineShopInvoiceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_invoice_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopInvoiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopInvoiceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexlasota/online_shop_invoice_service/OnlineShopInvoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_invoice_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopInvoiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
